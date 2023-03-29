8d711d161a587cdc39607634fd5c79f7e82cf378
bryanhclark <bryanhclark@gmail.com>
2023-03-29 18:13:45 -0400

    MISC

--- src/ui.rs
+++ src/ui.rs
@@ -1,19 +1,31 @@
 use tui::backend::Backend;
-use tui::widgets::{Block, BorderType, Borders};
+use tui::style::{Color,  Style};
+use tui::widgets::{Block,  Borders};
 use tui::{layout::Alignment, Frame};
 
 use crate::games::CardGame;
+
+pub struct MyList<T> {
+    items: Vec<T> 
+}
+
+struct MyListItem<'a> {
+}
+
+static MY_LIST: Vec<(&str, i32)> = [
+    ("ItemO", 1),
+    ("Item1", 2),
+    ("Item2", 3),
+];
 
 pub fn ui<B: Backend>(f: &mut Frame<B>, card_game: &CardGame) {
     let size = f.size();
 
     let block = Block::default()
         .borders(Borders::ALL)
-        .title("Main Block with round corners")
+        .title("ToDos")
         .title_alignment(Alignment::Center)
-        .border_type(BorderType::Rounded);
+        .style(Style::default().fg(Color::Yellow).bg(Color::Black));
+
     f.render_widget(block, size);
-    for card in card_game.draw_pile[0..4].iter()  {
-        println!("{:?}", card)
-    }
 }
