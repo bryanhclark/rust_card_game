10d01ff09868f5be9a605fba128b54e3dc50c794
bryanhclark <bryanhclark@gmail.com>
2023-03-29 18:22:38 -0400

    cleanup

--- src/ui.rs
+++ src/ui.rs
@@ -1,22 +1,10 @@
 use tui::backend::Backend;
+use tui::layout::Rect;
 use tui::style::{Color,  Style};
 use tui::widgets::{Block,  Borders};
 use tui::{layout::Alignment, Frame};
 
 use crate::games::CardGame;
-
-pub struct MyList<T> {
-    items: Vec<T> 
-}
-
-struct MyListItem<'a> {
-}
-
-static MY_LIST: Vec<(&str, i32)> = [
-    ("ItemO", 1),
-    ("Item1", 2),
-    ("Item2", 3),
-];
 
 pub fn ui<B: Backend>(f: &mut Frame<B>, card_game: &CardGame) {
     let size = f.size();
